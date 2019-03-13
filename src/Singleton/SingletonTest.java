7d27d022b52313f00c6ff4d5ecb7f1a10475eec1
bg <bharath.gandluru@gmail.com>
2019-03-13 10:39:56 -0700

    Singleton Enum

--- src/Singleton/SingletonTest.java
+++ src/Singleton/SingletonTest.java
@@ -5,8 +5,10 @@ public class SingletonTest {
   public static void main(String args[]) {
    //Static Inner class singleton
    System.out.println(SingletonSaticInnerClass.getInstance());
-    System.out.println(SingletonSaticInnerClass.getInstance());
-
+   System.out.println(SingletonSaticInnerClass.getInstance());
+   // Singleton ENUM
+   System.out.println(SingletonEnum.Insatance.hashCode());
+   System.out.println(SingletonEnum.Insatance.hashCode());
 
   }
 }
